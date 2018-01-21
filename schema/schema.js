25670c268be739515dcfdf57f611d61caf554bab
Tyler Ipson <tyler.ipson@clearlink.com>
2018-01-21 14:10:53 -0700

    Working graphiql with simple nested/reference points

---
+++ schema/schema.js
@@ -0,0 +1,81 @@
+const graphql = require('graphql');
+const axios = require('axios');
+
+const { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLSchema } = graphql;
+const api = 'http://localhost:3000';
+
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  fields: () => ({
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parentValue) {
+        return axios.get(`${api}/authors/${parentValue.authorId}`).then(res => res.data);
+      },
+    },
+  }),
+});
+
+const PageType = new GraphQLObjectType({
+  name: 'Page',
+  fields: () => ({
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parentValue) {
+        return axios.get(`${api}/authors/${parentValue.authorId}`).then(res => res.data);
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve(parentValue) {
+        return axios.get(`${api}/authors/${parentValue.id}/posts`).then(res => res.data);
+      },
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    post: {
+      type: PostType,
+      args: {
+        id: {
+          type: GraphQLString,
+        },
+      },
+      resolve(parentValue, args) {
+        return axios.get(`${api}/posts/${args.id}`).then(res => res.data);
+      },
+    },
+    page: {
+      type: PageType,
+      args: {
+        id: {
+          type: GraphQLString,
+        },
+      },
+      resolve(parentValue, args) {
+        return axios.get(`${api}/pages/${args.id}`).then(res => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
